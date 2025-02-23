e740c11fa3e4b3786eb47d7a9d27dd9d112a664c
aydinserbest <101752975+aydinserbest@users.noreply.github.com>
2025-02-23 23:49:10 +0100

    update

--- utils/fixtures.ts
+++ utils/fixtures.ts
@@ -1,7 +1,7 @@
 import { test as base } from '@playwright/test';
 import { RequestHandler } from './request-handlers';
 
-export const test = base.extend ({ 
+export const test = base.extend<{ api: RequestHandler }>({  // ✅ TypeScript artık biliyor!
     api: async ({}, use) => {
         const requestHandler = new RequestHandler();
         await use(requestHandler);
